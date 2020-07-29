92f0137fde7de10191f50aecc16e32842c8dc030
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-07-29 11:15:57 +0530

    Floating action button added

--- app/src/main/java/com/swapnil/whatsapp_clone/MainActivity.java
+++ app/src/main/java/com/swapnil/whatsapp_clone/MainActivity.java
@@ -6,18 +6,21 @@ import androidx.viewpager.widget.ViewPager;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;;
 
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private Toolbar toolbar;
+    FloatingActionButton fab;
     viewPagerAdapter viewPagerAdapter;
     private int[] tabIcons = {
             R.drawable.ic_camera_alt_black_24dp
@@ -30,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout=findViewById(R.id.tab_layout);
         viewPager=findViewById(R.id.view_pager);
-        toolbar=findViewById(R.id.myToolbar);
+        toolbar=findViewById(R.id.toolbar);
+        fab=findViewById(R.id.fab_action);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Whatsapp");
         viewPagerAdapter=new viewPagerAdapter(getSupportFragmentManager());
@@ -39,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
         viewPager.setAdapter(viewPagerAdapter);
         setupTabIcons();
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                changeFabIcon(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     @Override
@@ -63,5 +83,24 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupTabIcons() {
         tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+    }
+    private void changeFabIcon(final int index){
+        fab.hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switch (index){
+                    case 0:fab.setImageDrawable(getDrawable(R.drawable.ic_camera_alt_black_24dp));
+                           break;
+                    case 1:fab.setImageDrawable(getDrawable(R.drawable.ic_message_black_24dp));
+                           break;
+                    case 2:fab.setImageDrawable(getDrawable(R.drawable.ic_camera_alt_black_24dp));
+                           break;
+                    case 3:fab.setImageDrawable(getDrawable(R.drawable.ic_call_black_24dp));
+                           break;
+                }
+                fab.show();
+            }
+        },400);
     }
 }
